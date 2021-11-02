cb9c7d5933345d194f55d7893b84a37cf67617b8
Valentin W <vwojtasinski@insitaction.com>
2021-11-02 14:52:22 +0100

    build: args parser working

--- ping-pong/src/ArgsParser.ts
+++ ping-pong/src/ArgsParser.ts
@@ -1,10 +1,30 @@
 import { isIPv4 } from 'net';
 
-interface IArgsParser {
+export interface IArgsParser {
+  /**
+   * Le programme a-t-il été appelé en tant que Serveur ?
+   * Le programme a été appelé en tant que serveur si l'argument "server" est présent au moins une fois sur la ligne de commande.
+   *
+   * @returns {boolean} True si oui, Faux sinon
+   * @memberof IArgsParser
+   */
   isServer(): boolean;
-
+  /**
+   * Renvoie le numéro de port sur lequel écouter les connexions entrantes
+   * La valeur est le premier nombre compris entre 10000 et 65535 qui aura été éventuellement transmis sur la ligne de commande.
+   * Si aucune valeur transmise, la valeur par défaut est 23456
+   *
+   * @returns {number}
+   * @memberof IArgsParser
+   */
   getListeningPort(): number;
-
+  /**
+   * Renvoie la première adresse IPv4 transmise sur la ligne de commande.
+   * Si aucune adresse IPv4 n'a été transmise sur la ligne de commande, renvoyer FALSE
+   *
+   * @returns { number | false }
+   * @memberof IArgsParser
+   */
   getAddress(): string | false;
 }
 
@@ -36,16 +56,8 @@ export class ArgsParser implements IArgsParser {
   }
 
   getAddress(): string | false {
-    if (!this.isServer()) {
-      const address = this.args.find((arg) => isIPv4(arg));
+    const address = this.args.find((arg) => isIPv4(arg));
 
-      if (!address) {
-        return false;
-      }
-
-      return address;
-    }
-
-    return false;
+    return address || false;
   }
 }
--- ping-pong/src/index.ts
+++ ping-pong/src/index.ts
@@ -7,11 +7,16 @@ const isServer = argsParser.isServer();
 const address = argsParser.getAddress();
 const port = argsParser.getListeningPort();
 
-if (!isServer && !address) {
-  console.log('Veuillez renseigner une adresse cible');
-  process.exit();
+if (isServer) {
+  console.log(`Try listening on 127.0.0.1:${port}`);
+} else {
+  if (address) {
+    console.log(`You are pinging ${address}`);
+  } else {
+    console.error('Please specify a correct IPv4 address');
+  }
 }
 
-console.log({ isServer });
-console.log({ address });
-console.log({ port });
+// console.log({ isServer });
+// console.log({ address });
+// console.log({ port });
